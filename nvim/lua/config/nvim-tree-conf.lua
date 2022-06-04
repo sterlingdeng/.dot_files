e23976fe8299283b1a68ca3f49ab5dc58a9f828a
Sterling <sterlingdeng@gmail.com>
2022-06-03 22:36:03 -0700

    Update nvim-tree-conf.lua

--- nvim/lua/config/nvim-tree-conf.lua
+++ nvim/lua/config/nvim-tree-conf.lua
@@ -1,52 +1,70 @@
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git= {
-   unstaged = "✗",
-   staged = "✓",
-   unmerged = "",
-   renamed = "➜",
-   untracked = "★",
-   deleted = "",
-   ignored = "◌"
-  },
-  folder = {
-   arrow_open = "",
-   arrow_closed = "",
-   default = "",
-   open = "",
-   empty = "",
-   empty_open = "",
-   symlink = "",
-   symlink_open = "",
-   }
-}
-
-vim.cmd [[
-  let g:nvim_tree_git_hl = 1 "0 by default, will enable file highlight for git attributes (can be used without the icons).
-  let g:nvim_tree_highlight_opened_files = 1 "0 by default, will enable folder and file icon highlight for opened files/directories.
-  let g:nvim_tree_root_folder_modifier = ':~' "This is the default. See :help filename-modifiers for more options
-  let g:nvim_tree_add_trailing = 1 "0 by default, append a trailing slash to folder names
-  let g:nvim_tree_group_empty = 1 " 0 by default, compact folders that only contain a single folder into one node in the file tree
-  let g:nvim_tree_icon_padding = ' ' "one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-  let g:nvim_tree_symlink_arrow = ' >> ' " defaults to ' ➛ '. used as a separator between symlinks' source and target.
-  let g:nvim_tree_respect_buf_cwd = 1 "0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-  let g:nvim_tree_create_in_closed_folder = 1 "0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
-  let g:nvim_tree_special_files = { 'README.md': 1, 'Makefile': 1, 'MAKEFILE': 1, 'go.mod': 1, } " List of filenames that gets highlighted with NvimTreeSpecialFile
-  let g:nvim_tree_show_icons = {
-      \ 'git': 1,
-      \ 'folders': 1,
-      \ 'files': 1,
-      \ 'folder_arrows': 1,
-      \ }
-]]
-
-require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
-  open_on_setup_file = false,
-  view = {
-    side = "right",
-    number = true,
-    relativenumber = true,
+require'nvim-tree'.setup {
+	open_on_setup_file = false,
+	create_in_closed_folder = true,
+	respect_buf_cwd = true,
+	view = {
+		side = "right",
+		number = true,
+		relativenumber = true,
 		width = 45,
-  },
-} -- END_DEFAULT_OPTS
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		icons = {
+			hint =  "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	
+	renderer = {
+		group_empty = true,
+		highlight_opened_files = "none",
+		highlight_git = true,
+		root_folder_modifier = ':~',
+		add_trailing = true,
+		icons = {
+			symlink_arrow = ' >> ', -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+			glyphs = {
+				default = "",
+				symlink = "",
+				git= {
+				 unstaged = "✗",
+				 staged = "✓",
+				 unmerged = "",
+				 renamed = "➜",
+				 untracked = "★",
+				 deleted = "",
+				 ignored = "◌"
+				},
+				folder = {
+				 arrow_open = "",
+				 arrow_closed = "",
+				 default = "",
+				 open = "",
+				 empty = "",
+				 empty_open = "",
+				 symlink = "",
+				 symlink_open = "",
+				 },
+			},
+			show = {
+				git = true,
+				file = true,
+				folder = true,
+				folder_arrow = true,
+			},
+		},
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+		special_files = { 'README.md', 'Makefile', 'MAKEFILE', 'go.mod' }
+	},
+}
